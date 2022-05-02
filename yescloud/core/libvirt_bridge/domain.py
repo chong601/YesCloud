bd7de82b0f7be57452a33903656da9c3eecbda94
Chong Jin Yi <chong601.13@gmail.com>
2022-05-02 18:22:23 +0800

    Use UUID for already-existed domains

--- yescloud/core/libvirt_bridge/domain.py
+++ yescloud/core/libvirt_bridge/domain.py
@@ -11,42 +11,42 @@ def list_domains(flag: int):
     return result
 
 @handle_libvirt_exception
-def get_domain_detail(vm_id):
+def get_domain_detail(vm_uuid):
     """
     Get a domain information
     
     Might consider getting a full summary of the 
     """
-    domain = conn_ro.lookupByID(vm_id)
+    domain = conn_ro.lookupByID(vm_uuid)
     return domain.info()
 
 @handle_libvirt_exception
-def create_domain(vm_id):
+def create_domain(vm_uuid):
     """
     Start a domain
     """
-    domain = conn_rw.lookupByID(vm_id)
+    domain = conn_rw.lookupByID(vm_uuid)
     return True if not domain.create() else False
 
 @handle_libvirt_exception
-def destroy_domain(vm_id):
+def destroy_domain(vm_uuid):
     """
     Hard power-off a domain
     This is similar to yanking the power cord or long-press the power button
     """
-    domain = conn_rw.lookupByID(vm_id)
+    domain = conn_rw.lookupByID(vm_uuid)
     return True if not domain.destroy() else False
 
 @handle_libvirt_exception
-def shutdown_domain(vm_id):
+def shutdown_domain(vm_uuid):
     """
     Shut down a domain using graceful shutdown ala power-button press or hrough the OS
     """
-    domain = conn_rw.lookupByID(vm_id)
+    domain = conn_rw.lookupByID(vm_uuid)
     return True if not domain.shutdown() else False
 
 @handle_libvirt_exception
-def undefine_domain(vm_id):
+def undefine_domain(vm_uuid):
     """
     Delete a domain
     undefine is the libvirt-speak for deleting the entry.
@@ -54,7 +54,7 @@ def undefine_domain(vm_id):
     Note that undefining a domain does not remove all data owned by the domain;
     it's up to the developer to decide how to remove them
     """
-    domain = conn_rw.lookupByID(vm_id)
+    domain = conn_rw.lookupByID(vm_uuid)
     return True if not domain.undefine() else False
 
 @handle_libvirt_exception
