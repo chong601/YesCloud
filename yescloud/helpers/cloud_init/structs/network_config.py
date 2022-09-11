68e428b7709f68f87c853cbc128de6ebacd33e6e
Chong Jin Yi <chong601.13@gmail.com>
2022-09-11 17:24:11 +0800

    I'm back. Add some boring Cloud-Init very rough implementation

---
+++ yescloud/helpers/cloud_init/structs/network_config.py
@@ -0,0 +1,25 @@
+from .users import User
+
+
+class NetworkConfig(object):
+    """
+    Base representation for the Cloud-Init object to be exported to YAML
+    """
+    _attr = {}
+
+    def __init__(self):
+        # Initialize base Cloud-Init configuration
+        ethernets_config = {
+        }
+        self._attr.update({'version': 2})
+        self._attr.update({'ethernets': ethernets_config})
+
+    def addInterface(self, network_config: dict):
+        count = len(self._attr.get('ethernets').keys())
+        self._attr.get('ethernets').update({''.join(['enp', str(count+1), 's0']): network_config})
+
+    def getConfig(self):
+        return self._attr
+
+    def __repr__(self):
+        return str(self._attr)
---
+++ yescloud/helpers/cloud_init/structs/root.py
@@ -0,0 +1,67 @@
+from .users import User
+
+
+class Root(object):
+    """
+    Base representation for the Cloud-Init object to be exported to YAML
+    """
+    _attr = {}
+    _mapper_attr = {
+        'users': User()
+    }
+
+    def __init__(self):
+        # Initialize base Cloud-Init configuration
+        self._attr.update({'system_info': {'default_user': {'name': 'ubuntu'}}})
+        self._attr.update({'package_update': True})
+        self._attr.update({'package_upgrade': True})
+        self._attr.update({'chpasswd': {'expire': False}})
+        self._attr.update({'ssh_pwauth': True})
+        self._attr.update({'hostname': 'ubuntu'})
+        self._attr.update({'timezone': 'Etc/UTC'})
+        self._attr.update({'packages': ['qemu-guest-agent', 'haveged']})
+
+    # def __setattr__(self, name: str, value: Any) -> None:
+    #     self._attr.update({name: value})
+    #
+    # def __getattribute__(self, name: str) -> Any:
+    #     self._attr.get(name)
+    #
+    # def __delattr__(self, name: str) -> None:
+    #     self._attr.pop(name)
+    def setHostname(self, hostname):
+        self._attr.update({'hostname': hostname})
+
+    def setDefaultUserPassword(self, password):
+        self._attr.update({'ssh_pwauth': password})
+
+    def setUsers(self, users):
+        self._attr.update({'users': users})
+
+    def enablePackageUpdate(self):
+        self._attr.update({'package_update': True})
+
+    def disablePackageUpdate(self):
+        self._attr.update({'package_update': False})
+
+    def addPackages(self, packages):
+        if isinstance(packages, str):
+            self._attr.get('packages').append(packages)
+        elif isinstance(packages, list):
+            self._attr.get('packages').extend(packages)
+
+    def addSSHKeyToDefaultUser(self, ssh_key):
+        ssh_key_array: list = self._attr.get('system_info').get('default_user').get('ssh_authorized_keys')
+        if ssh_key_array is None:
+            ssh_key_array = []
+        if isinstance(ssh_key, list):
+            ssh_key_array.extend(ssh_key)
+        elif isinstance(ssh_key, str):
+            ssh_key_array.append(ssh_key)
+        self._attr.get('system_info').get('default_user').update({'ssh_authorized_keys': ssh_key_array})
+
+    def getConfig(self):
+        return self._attr
+
+    def __repr__(self):
+        return str(self._attr)
