90a99cd793ceddec079cb19ffe5e3582d19ad610
Chong Jin Yi <chong601.13@gmail.com>
2022-09-12 07:31:01 +0800

    Add a skeleton of domain installation

---
+++ yescloud/cli/install.py
@@ -0,0 +1,7 @@
+from yescloud.helpers.cloud_init.structs.root import Root
+
+# TODO: We need a configparser
+
+# There are two ways to set up a VM:
+# Either you run virt-install (ew) or you generate the XML file and pass it into libvirt to be defined in (ewwwww)
+
